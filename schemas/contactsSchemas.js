ffc14bf4943cb1c672425bc32c55297b914005b6
Voslo <rewqvolty228@gmail.com>
2024-04-08 20:18:48 +0300

    finish?

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -4,10 +4,10 @@ export const createContactSchema = Joi.object({
     name: Joi.string().required(),
     email: Joi.string().email().required(),
     phone: Joi.string().required()
-});
+})
 
 export const updateContactSchema = Joi.object({
     name: Joi.string(),
     email: Joi.string().email(),
     phone: Joi.string()
-}).or('name', 'email', 'phone');
+})
